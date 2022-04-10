d67254d59021b52b6349bec3870aee8a811d0ab3
z3zzz <zabra7946@naver.com>
2022-04-10 09:08:40 +0900

    add: react mouseEvent example

--- src/events/EventComponent.tsx
+++ src/events/EventComponent.tsx
@@ -1,8 +1,14 @@
 const EventComponent: React.FC = () => {
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>): void =>
-    console.log(e);
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    console.log("i am changed", e);
+  };
+
   const onDragStart = (e: React.DragEvent<HTMLDivElement>) => {
     console.log("i am dragged", e);
+  };
+
+  const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    console.log("i am clicked", e);
   };
 
   return (
@@ -11,6 +17,7 @@ const EventComponent: React.FC = () => {
       <div draggable onDragStart={onDragStart}>
         Drage Me
       </div>
+      <button onClick={onClick}>Click Me</button>
     </div>
   );
 };
